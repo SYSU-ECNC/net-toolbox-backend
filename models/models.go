cf0b4d73d2b0ab783d13e93f499fb3bef2c137e4
chenzhg <2216468566@qq.com>
2022-03-23 00:40:19 +0800

    修改函数命名方式为camelCase方式

--- models/models.go
+++ models/models.go
@@ -8,7 +8,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func ConnectDB() *sql.DB {
+func connectDB() *sql.DB {
 	host, port, user, password, dbname := config.GetDBHost(), config.GetDBPort(), config.GetDBUser(), config.GetDBPassword(), config.GetDBName()
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
@@ -22,11 +22,10 @@ func ConnectDB() *sql.DB {
 	return db
 }
 
-var DB = ConnectDB()
+var DB = connectDB()
 
 //将第一次登录的用户的信息写入数据库
 func DatabaseWrite(name, Union_id string) {
-	// db := ConnectDB()
 	_, err := DB.Exec("insert into ecncer(name,union_id) values($1,$2)", name, Union_id)
 	if err != nil {
 		panic(err)
@@ -35,7 +34,6 @@ func DatabaseWrite(name, Union_id string) {
 
 // 判断用户是否已存在
 func IsExist(Union_id string) bool {
-	// db := ConnectDB()
 	row, err := DB.Query(`SELECT count(*) from ecncer where union_id = $1`, Union_id)
 	if err == nil {
 		var count int
@@ -46,12 +44,10 @@ func IsExist(Union_id string) bool {
 			}
 		}
 	}
-	// defer DB.Close()
 	return false
 }
 
 func GetUserNameFromDB(UnionID string) string {
-	// db := ConnectDB()
 	row, err := DB.Query(`SELECT name from ecncer where union_id = $1`, UnionID)
 	if err == nil {
 		var name string
