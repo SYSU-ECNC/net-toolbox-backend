3745e1c0eaf00b42476706c40612646f3d4e05ec
chenzhg <2216468566@qq.com>
2022-03-22 15:07:49 +0800

    main文件和router的修改

--- router/router.go
+++ router/router.go
@@ -1,17 +1,28 @@
 package router
 
 import (
+	"net/http"
 	"toolBox/user"
 
 	"github.com/gin-gonic/gin"
 )
 
+// 模拟前端主页页面
+func frontendHome(c *gin.Context) {
+	c.String(http.StatusOK, "Hello, here is the home!")
+}
+
 func SetupRouters() *gin.Engine {
 	r := gin.Default()
 
+	// 登录页
 	r.GET("/login", user.Login)
+	// 飞书跳转后端url
 	r.GET("/callback/path", user.Callback)
-	r.GET("/home", user.Home)
+	// 模拟前端主页页面
+	r.GET("/home", frontendHome)
+	// r.GET("/home", user.Home)
+	// 返回json对象
 	r.GET("/getUserName", user.GetUserName)
 	return r
 }
