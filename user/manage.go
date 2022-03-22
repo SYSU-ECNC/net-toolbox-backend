32a13ed08fbac90040981609ddecf529661d7505
chenzhg <2216468566@qq.com>
2022-03-22 15:01:31 +0800

    实现getUserName接口，实现用户认证接口

--- user/manage.go
+++ user/manage.go
@@ -7,31 +7,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//验证身份，无效直接返回响应重定向url，有效函数返回UnionID
-func VerifyIdentity(c *gin.Context) string {
-	token := c.Request.Header.Get("Authorization")
-	if !VerifyToken(token) {
-		redirect_url := "http://127.0.0.1:8888/login"
-		c.JSON(http.StatusFound, gin.H{"msg": "wrong token!", "redirect_url": redirect_url})
-	}
-	_, claims, _ := ParseToken(token)
-	return claims.Id
-}
-
+//前端接收到的要么是200状态码，要么是401状态码
 func GetUserName(c *gin.Context) {
-	UnionID := VerifyIdentity(c)
-	name := models.GetUserName(UnionID)
-	c.JSON(http.StatusOK, gin.H{"name": name})
+	UnionID := GetIDFromCookie(c)
+
+	// UnionID为空说明cookie不合法，用户处于未登录状态，返回重定向登录url，前端实现重定向
+	if UnionID == "" {
+		return
+	}
+
+	name := models.GetUserNameFromDB(UnionID)
+	c.JSON(http.StatusOK, gin.H{"username": name})
 }
 
-func Home(c *gin.Context) {
-	// token := c.Request.Header.Get("Authorization")
-	// if !VerifyToken(token) {
-	// 	redirect_url := "http://127.0.0.1:8888/login"
-	// 	c.JSON(http.StatusFound, gin.H{"msg": "wrong token!", "redirect_url": redirect_url})
-	// }
-	// _, claims, _ := ParseToken(token)
-	// open_id := claims.Id
-	Union_id := VerifyIdentity(c)
-	c.JSON(http.StatusOK, gin.H{"open_id": Union_id})
-}
+// //验证身份，无效直接返回响应重定向url，有效函数返回UnionID
+// func VerifyIdentity(c *gin.Context) string {
+// 	token := c.Request.Header.Get("Authorization")
+// 	if !VerifyToken(token) {
+// 		redirect_url := "http://127.0.0.1:8888/login"
+// 		c.JSON(http.StatusFound, gin.H{"msg": "wrong token!", "redirect_url": redirect_url})
+// 	}
+// 	_, claims, _ := ParseToken(token)
+// 	return claims.Id
+// }
+
+// func GetUserName(c *gin.Context) {
+// 	UnionID := VerifyIdentity(c)
+// 	name := models.GetUserName(UnionID)
+// 	c.JSON(http.StatusOK, gin.H{"name": name})
+// }
+
+// func Home(c *gin.Context) {
+// 	Union_id := VerifyIdentity(c)
+// 	c.JSON(http.StatusOK, gin.H{"open_id": Union_id})
+// }
