7273dab32390115eb67965153a5d6d46ab99074d
chenzhg <2216468566@qq.com>
2022-03-23 13:40:05 +0800

    修改urlPath为RESTful风格

--- router/router.go
+++ router/router.go
@@ -18,11 +18,11 @@ func SetupRouters() *gin.Engine {
 	// 登录页
 	r.GET("/login", user.Login)
 	// 飞书跳转后端url
-	r.GET("/callback/path", user.Callback)
+	r.GET("/callback", user.Callback)
 	// 模拟前端主页页面
 	r.GET("/home", frontendHome)
 	// r.GET("/home", user.Home)
 	// 返回json对象
-	r.GET("/getUserName", user.GetUserName)
+	r.GET("/get/user/me", user.GetUserName)
 	return r
 }
