bda15406374657be79410b0a9677e11dfbe4ad70
chenzhg <2216468566@qq.com>
2022-03-24 17:39:54 +0800

    修改appAccessToken缓存过期的bug，改为需要时再获取

--- user/larkLogin.go
+++ user/larkLogin.go
@@ -13,18 +13,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 实现tenantAccessToken缓存
-var tenantAccessToken string
-
-func init() {
-	tenantAccessToken = getAppAccessToken()
-}
+// 命名为conf是为了避免和*gin.Context的变量重名
+var conf config.Config = config.GetConfig()
 
 func getAppAccessToken() string {
-	app_id, app_secret := config.GetAPPID(), config.GetAPPSecret()
-	data := url.Values{"app_id": {app_id}, "app_secret": {app_secret}}
-	_url := "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
-	resp, err := http.PostForm(_url, data)
+	appId, appSecret := conf.App.Id, conf.App.Secret
+	// 此处的key命名为app_id是飞书接口的要求
+	data := url.Values{"app_id": {appId}, "app_secret": {appSecret}}
+	accessUrl := "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
+	resp, err := http.PostForm(accessUrl, data)
 	var match string
 	if err == nil {
 		data, _ := ioutil.ReadAll(resp.Body)
@@ -42,24 +39,30 @@ func getCode(c *gin.Context) string {
 	return ret
 }
 
-type Payload struct {
-	Grant_type string `json:"grant_type"`
-	Code       string `json:"code"`
+// 此处结构体成员变量命名首字母不大写就会有警告
+// 因为结构体转json存储时，结构体成员变量首字母必须大写才能成功输出
+// 所以只能写成大驼峰形式
+type payload struct {
+	GrantType string `json:"grant_type"`
+	Code      string `json:"code"`
 }
 
-func getUserMessage(_code string) string {
-	_url := "https://open.feishu.cn/open-apis/authen/v1/access_token"
+func getUserMessage(userCode string) string {
+	url := "https://open.feishu.cn/open-apis/authen/v1/access_token"
 
-	payload := Payload{
-		Grant_type: "authorization_code",
-		Code:       _code,
+	payloadData := payload{
+		GrantType: "authorization_code",
+		Code:      userCode,
 	}
-	data, _ := json.Marshal(payload)
+
+	data, _ := json.Marshal(payloadData)
 	client := &http.Client{}
-	req, _ := http.NewRequest("POST", _url, bytes.NewReader(data))
+	req, _ := http.NewRequest("POST", url, bytes.NewReader(data))
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("Authorization", "Bearer "+tenantAccessToken)
+	// 暂时还是改为每次登录再请求飞书获得AppAccessToken
+	req.Header.Set("Authorization", "Bearer "+getAppAccessToken())
 	resp, err := client.Do(req)
+
 	var respBody []byte
 	if err == nil {
 		defer resp.Body.Close()
@@ -70,57 +73,62 @@ func getUserMessage(_code string) string {
 }
 
 func Login(c *gin.Context) {
-	app_id := config.GetAPPID()
-	callbackUrl := config.GetCallbackUrl()
-	redirectUrl := `https://open.feishu.cn/open-apis/authen/v1/index?redirect_uri=` + callbackUrl + `&app_id=` + app_id
+	appId := conf.App.Id
+	callbackUrl := conf.PublicUrl.CallbackUrl
+	redirectUrl := `https://open.feishu.cn/open-apis/authen/v1/index?redirect_uri=` + callbackUrl + `&app_id=` + appId
 	c.Redirect(http.StatusFound, redirectUrl)
 }
 
-type Data struct {
-	Access_token       string `json:"access_token"`
-	Token_type         string `json:"token_type"`
-	Expires_in         int    `json:"expires_in"`
-	Name               string `json:"name"`
-	En_name            string `json:"en_name"`
-	Avatar_url         string `json:"avatar_url"`
-	Avatar_thumb       string `json:"avatar_thumb"`
-	Avatar_middle      string `json:"avatar_middle"`
-	Avatar_big         string `json:"avatar_big"`
-	Open_id            string `json:"open_id"`
-	Union_id           string `json:"union_id"`
-	Email              string `json:"email"`
-	User_id            string `json:"user_id"`
-	Mobile             string `json:"mobile"`
-	Tenant_key         string `json:"tenant_key"`
-	Refresh_expires_in int    `json:"refresh_expires_in"`
-	Refresh_token      string `json:"refresh_token"`
+// 此处结构体成员变量命名首字母不大写就会有警告
+// 因为结构体转json存储时，结构体成员变量首字母必须大写才能成功输出
+// 所以只能写成大驼峰形式
+// UserData是通过用户的code向飞书请求的该用户的信息
+type UserData struct {
+	AcessToken       string `json:"access_token"`
+	TokenType        string `json:"token_type"`
+	ExpiresIn        int    `json:"expires_in"`
+	Name             string `json:"name"`
+	EnName           string `json:"en_name"`
+	AvatarUrl        string `json:"avatar_url"`
+	AvatarThumb      string `json:"avatar_thumb"`
+	AvatarMiddle     string `json:"avatar_middle"`
+	AvatarBig        string `json:"avatar_big"`
+	OpenId           string `json:"open_id"`
+	UnionId          string `json:"union_id"`
+	Email            string `json:"email"`
+	UserId           string `json:"user_id"`
+	Mobile           string `json:"mobile"`
+	TenantKey        string `json:"tenant_key"`
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	RefreshToken     string `json:"refresh_token"`
 }
 
 type User struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data Data   `json:"data"`
+	Code     int      `json:"code"`
+	Msg      string   `json:"msg"`
+	UserData UserData `json:"data"`
 }
 
 func Callback(c *gin.Context) {
 	code := getCode(c)
+
 	userMsg := getUserMessage(code)
 	var user User
 	err := json.Unmarshal([]byte(userMsg), &user)
 	var name string
-	var UnionID string
+	var unionID string
 	if err == nil {
-		name = user.Data.Name
-		// Union_id是飞书账号的唯一标识
-		UnionID = user.Data.Union_id
-		if !models.IsExist(UnionID) {
+		name = user.UserData.Name
+		// unionID是飞书账号的唯一标识
+		unionID = user.UserData.UnionId
+		if !models.IsExist(unionID) {
 			// 用户不存在，在数据库中写入用户信息
-			models.AddUser(name, UnionID)
+			models.AddUser(name, unionID)
 		}
-		SetCookie(c, UnionID, name)
-		redirect_url := config.GetFrontendUrl()
+		SetCookie(c, unionID, name)
+		redirect_url := conf.PublicUrl.FrontentUrl
 		c.Redirect(http.StatusFound, redirect_url)
 	} else {
-		c.Redirect(http.StatusInternalServerError, config.GetLoginUrl())
+		c.Redirect(http.StatusInternalServerError, conf.PublicUrl.LoginUrl)
 	}
 }
