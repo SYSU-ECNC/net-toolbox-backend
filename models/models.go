6b658b17124f1260dd3da921529f085719125769
chenzhg <2216468566@qq.com>
2022-03-27 20:51:59 +0800

    添加了创建agent的函数

--- models/models.go
+++ models/models.go
@@ -1,3 +1,4 @@
+// 数据库命名都是下划线风格，因为postgreSQL对字段名大小写不敏感
 package models
 
 import (
@@ -8,11 +9,13 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// 命名为conf是为了避免和*gin.Context的变量重名
+var conf config.Config = config.GetConfig()
+
 func connectDB() *sql.DB {
-	host, port, user, password, dbname := config.GetDBHost(), config.GetDBPort(), config.GetDBUser(), config.GetDBPassword(), config.GetDBName()
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		conf.Database.Host, conf.Database.Port, conf.Database.User, conf.Database.Password, conf.Database.Dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -59,4 +62,25 @@ func GetUserNameFromDB(UnionID string) string {
 		}
 	}
 	return ""
+}
+
+func GetAgentTokenFromDB(name string) string {
+	row, err := DB.Query(`SELECT token from agent where name = $1`, name)
+	if err == nil {
+		var token string
+		for row.Next() {
+			err1 := row.Scan(&token)
+			if err1 == nil {
+				return token
+			}
+		}
+	}
+	return ""
+}
+
+func AddAgent(name, token string) {
+	_, err := DB.Exec("insert into agent(name,token) values($1,$2)", name, token)
+	if err != nil {
+		panic(err)
+	}
 }
