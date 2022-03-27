587042e19c5d74f5b6585f8faa361607e2f31a7f
chenzhg <2216468566@qq.com>
2022-03-27 20:49:57 +0800

    实现agent的验证和token的生成

--- agent/agent.go
+++ agent/agent.go
@@ -1,35 +1,67 @@
-//尚无进展
-
 package agent
 
 import (
+	"io/ioutil"
+	"math/rand"
+	"net/http"
 	"time"
+	"toolBox/models"
 
+	"github.com/bitly/go-simplejson"
 	"github.com/gin-gonic/gin"
-	"gopkg.in/dgrijalva/jwt-go.v3"
 )
 
-type Claims struct {
-	name       string `json:"name"`
-	masterIP   string `json:"masterIP"`
-	masterPort string `json:"masterPort"`
-	jwt.StandardClaims
+var token string
+
+func init() {
+	// 生成32位的token
+	token = generateToken(32)
 }
 
-func AgentRegister(c *gin.Context) {
-	// name := c.PostForm("name")
-	// masterIP := c.PostForm("masterIP")
-	// masterPort := c.PostForm(("masterPost"))
-	claims := &Claims{
-		name:       c.PostForm("name"),
-		masterIP:   c.PostForm("masterIP"),
-		masterPort: c.PostForm("masterPost"),
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expire.Unix(),
-			IssuedAt:  time.Now().Unix(),
-			// Issuer:    "localhost",
-			// Subject:   "user token",
-		},
+// 生成token
+func generateToken(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
-	expire := time.Now().Add(2 * time.Hour)
+	return string(b)
+}
+
+func GetToken(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"registration-token": token})
+}
+
+func CheckToken(c *gin.Context) {
+	request := c.Request
+	request.ParseForm()
+	name, agentToken := request.Form["name"], request.Form["token"]
+	dbToken := models.GetAgentTokenFromDB(name[0])
+	if agentToken[0] == token || agentToken[0] == dbToken {
+		// 如果dbToken为空，说明是新注册的agent
+		if dbToken == "" {
+			models.AddAgent(name[0], token)
+		}
+
+	} else {
+		// token非法,阻止访问agentApi
+		c.Abort()
+	}
+}
+
+func GetTask(c *gin.Context) {
+	// 测试阶段
+	c.JSON(http.StatusOK, gin.H{"cmd": "ping", "target": "www.baidu.com"})
+}
+
+// 需要提交的body为json，分别有agentName、cmd、target三个键
+func AddTaskByUser(c *gin.Context) {
+	body, _ := ioutil.ReadAll(c.Request.Body)
+	jsonObj, _ := simplejson.NewJson(body)
+	agentName, _ := jsonObj.Get("agentName").String()
+	cmd, _ := jsonObj.Get("cmd").String()
+	target, _ := jsonObj.Get("target").String()
+
+	// return
 }
