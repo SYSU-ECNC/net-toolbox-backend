838723a1f56731ebd9f98533f5a1b8773854b970
chenzhg <2216468566@qq.com>
2022-04-25 15:47:22 +0800

    feat:实现GET /user/agent/token接口

--- agent/agent.go
+++ agent/agent.go
@@ -1,13 +1,10 @@
 package agent
 
 import (
-	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"time"
-	"toolBox/models"
 
-	"github.com/bitly/go-simplejson"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,38 +27,33 @@ func generateToken(n int) string {
 }
 
 func GetToken(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"registration-token": token})
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
-func CheckToken(c *gin.Context) {
-	request := c.Request
-	request.ParseForm()
-	name, agentToken := request.Form["name"], request.Form["token"]
-	dbToken := models.GetAgentTokenFromDB(name[0])
-	if agentToken[0] == token || agentToken[0] == dbToken {
-		// 如果dbToken为空，说明是新注册的agent
-		if dbToken == "" {
-			models.AddAgent(name[0], token)
-		}
+// func CheckToken(c *gin.Context) {
+// 	request := c.Request
+// 	request.ParseForm()
+// 	name, agentToken := request.Form["name"], request.Form["token"]
+// 	dbToken := models.GetAgentTokenFromDB(name[0])
+// 	if agentToken[0] == token || agentToken[0] == dbToken {
+// 		// 如果dbToken为空，说明是新注册的agent
+// 		if dbToken == "" {
+// 			models.AddAgent(name[0], token)
+// 		}
 
-	} else {
-		// token非法,阻止访问agentApi
-		c.Abort()
-	}
-}
-
-func GetTask(c *gin.Context) {
-	// 测试阶段
-	c.JSON(http.StatusOK, gin.H{"cmd": "ping", "target": "www.baidu.com"})
-}
+// 	} else {
+// 		// token非法,阻止访问agentApi
+// 		c.Abort()
+// 	}
+// }
 
 // 需要提交的body为json，分别有agentName、cmd、target三个键
-func AddTaskByUser(c *gin.Context) {
-	body, _ := ioutil.ReadAll(c.Request.Body)
-	jsonObj, _ := simplejson.NewJson(body)
-	agentName, _ := jsonObj.Get("agentName").String()
-	cmd, _ := jsonObj.Get("cmd").String()
-	target, _ := jsonObj.Get("target").String()
+// func AddTaskByUser(c *gin.Context) {
+// 	body, _ := ioutil.ReadAll(c.Request.Body)
+// 	jsonObj, _ := simplejson.NewJson(body)
+// 	agentName, _ := jsonObj.Get("agentName").String()
+// 	cmd, _ := jsonObj.Get("cmd").String()
+// 	target, _ := jsonObj.Get("target").String()
 
-	// return
-}
+// 	// return
+// }
